803756af4991dd9de518d8a810e089db4a45516e
Przemyslaw Cimcioch <cimcioch.przemyslaw@gmail.com>
2019-05-13 23:22:24 +0200

    Fixed basic auth configuration

--- src/main/java/rtc/signaling/SecurityConfiguration.java
+++ src/main/java/rtc/signaling/SecurityConfiguration.java
@@ -9,6 +9,7 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http.csrf().disable();
     }
 }
